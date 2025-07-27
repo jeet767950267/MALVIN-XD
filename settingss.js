92447b0f74cce7622c837bad7df2ddd026f8023a
jeet767950267 <jeet83733@gmail.com>
2025-07-28 03:16:13 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~gy1niBCL#LKkpfWNCLo5gR3IhoFPLKxCS60---8h39vr8apkRk9U",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
